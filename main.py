10e86df64ce97929b0bd3e2d6e01e1ef10dd0130
mo-kun <64027382+mo-kun@users.noreply.github.com>
2022-07-23 07:19:17 -1100

    rthhth

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ my_secret = "MTAwMDQyMzE5NDI5MDgxNTE4MA.GEnyJs.WVb_MyArpE8387RUNa2IIIjJeB2lSua1H
 async def on_message(message):
   if message.channel.id ==999823889653846060 and message.content.startswith("-suggest") : 
       if  len(message.content)>8:
-          await message.delete()
           embed = discord.Embed(    
           title="suggestion made by "+message.author.name,  description=message.content[9:], color = 0xf1c40f) 
           await message.channel.send(embed=embed)
-      
+          await message.delete()
+          
       elif 0<=len(message.content)<=8:
           await message.delete()
   elif  message.channel.id ==999823889653846060 and message.author!=bot.user:

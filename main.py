7f323f7f70d9fa414572e8883fa6c574ad252b5e
mo-kun <64027382+mo-kun@users.noreply.github.com>
2022-07-23 07:22:40 -1100

    zefr

--- main.py
+++ main.py
@@ -11,16 +11,16 @@ my_secret = "MTAwMDQyMzE5NDI5MDgxNTE4MA.GEnyJs.WVb_MyArpE8387RUNa2IIIjJeB2lSua1H
 
 @bot.listen()
 async def on_message(message):
-  if message.channel.id ==999823889653846060 and message.content.startswith("-suggest") : 
+  if message.channel.id ==1000467738738823319 and message.content.startswith("-suggest") : 
       if  len(message.content)>8:
           embed = discord.Embed(    
           title="suggestion made by "+message.author.name,  description=message.content[9:], color = 0xf1c40f) 
           await message.channel.send(embed=embed)
           await message.delete()
-          
+
       elif 0<=len(message.content)<=8:
           await message.delete()
-  elif  message.channel.id ==999823889653846060 and message.author!=bot.user:
+  elif  message.channel.id ==1000467738738823319 and message.author!=bot.user:
     await message.delete()
 
 
